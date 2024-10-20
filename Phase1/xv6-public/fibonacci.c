2e264676a7390227dca345ff7d44e74fa34b5c71
Pooria-mh <pmah3000@gmail.com>
2024-10-20 12:14:02 +0330

    FIbonacci command and some comments

---
+++ Phase1/xv6-public/fibonacci.c
@@ -0,0 +1,26 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+// Function to calculate Fibonacci numbers recursively
+int fibonacci(int n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void fibonacci_test(int limit) {
+    for (int i = 0; i < limit + 1; i++) {
+        printf(1, "Fibonacci of %d is %d\n", i, fibonacci(i));
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf(1, "Usage: fibonacci <limit>\n");
+        exit();
+    }
+    int limit = atoi(argv[1]);
+    fibonacci_test(limit);
+    exit();
+}
