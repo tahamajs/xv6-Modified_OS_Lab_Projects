15752a4628fb990055d9e82c4ee19ccc8f2bf4df
Pooria-mh <pmah3000@gmail.com>
2024-11-10 18:08:05 +0330

    Report Branch

--- Phase2/xv6-public/proc.h
+++ Phase2/xv6-public/proc.h
@@ -60,7 +60,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
     int syscalls[MAX_SYSCALLS];//Save the number of each system call
-    int syscall_count;         //SAving the total number of system calls
+    int syscall_count;         //Saving the total number of system calls
 
 };
 
