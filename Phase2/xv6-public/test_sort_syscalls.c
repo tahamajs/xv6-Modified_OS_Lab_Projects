b16220a67f1fe0e5e1d2aa012bf117e7c283e338
Pooria-mh <pmah3000@gmail.com>
2024-11-10 18:56:51 +0330

    Sort Syscalls Fixed

--- Phase2/xv6-public/test_sort_syscalls.c
+++ Phase2/xv6-public/test_sort_syscalls.c
@@ -4,20 +4,27 @@
 #include "stat.h"
 #include "user.h"
 
-int main(void) {
-    int pid = getpid();
-
-    // Make some system calls
-    printf(1, "Testing sort_syscalls\n");
-    getpid();
-    sleep(10);
-    write(1, "Hello\n", 6);
-
-    // Sort the system calls
-    if (sort_syscalls(pid) < 0) {
-        printf(2, "Failed to sort system calls\n");
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf(2, "Usage: test_sort_syscalls <pid>\n");
         exit();
     }
 
+    int pid = atoi(argv[1]);
+    if(pid == 0 && argv[1][0] != '0'){
+        printf(2, "Usage: test_sort_syscalls <pid>\n");
+        exit();
+    }
+    if(pid == getpid()){
+        printf(1, "Testing sort_syscalls\n");
+        getpid();
+        sleep(10);
+        write(1, "Hello\n", 6);
+    }
+    // Sort the system calls
+    if (sort_syscalls(pid) < 0) {
+        printf(2, "Requested pid was not found\n");
+        exit();
+    }
     exit();
 }
